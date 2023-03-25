0b37b44044e787e242fa4f13a1c797bbba9d2790
Constantine Cherkashyn <cons1@protonmail.ch>
2023-03-25 13:50:08 -0400

    Login and register

---
+++ client/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+
+return(
+    <div className="content">
+        Login form
+     </div>
+    )
+}
+export default Login;
---
+++ client/src/components/Register.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Register() {
+
+return(
+    <div className="content">
+        Registration form
+     </div>
+    )
+}
+export default Register;
