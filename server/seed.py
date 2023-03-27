a5d63f0150777f2d5efb63d71ff59dab028847f3
Eli Moshe <moshebogo@gmail.com>
2023-03-26 21:23:08 -0400

    added both prices

--- server/seed.py
+++ server/seed.py
@@ -16,8 +16,12 @@ with app.app_context():
     hours = [ '9:00-10:00','10:00-11:00','11:00-12:00','12:00-1:00','1:00-2:00','2:00-3:00','3:00-4:00','4:00-5:00','5:00-6:00',
               '9:30-10:30','10:30-11:30','11:30-12:30','12:30-1:30','1:30-2:30','2:30-3:30','3:30-4:30','4:30-5:30','5:30-6:30',]
     
-    boats = [{'name': 'kayak',      'capacity': 1}, {'name': 'jet-ski',      'capacity': 2}, {'name': 'catamaran',        'capacity': 4}, 
-             {'name': 'speed-boat', 'capacity': 5}, {'name': 'fishing-boat', 'capacity': 3}, {'name': 'swan-paddle-boat', 'capacity': 3},      ]
+    boats = [{'name': 'kayak',            'capacity': 1, 'wkday_p': 20,  'wkend_p': 30 },
+             {'name': 'jet-ski',          'capacity': 2, 'wkday_p': 150, 'wkend_p': 190}, 
+             {'name': 'catamaran',        'capacity': 4, 'wkday_p': 200, 'wkend_p': 250}, 
+             {'name': 'speed-boat',       'capacity': 5, 'wkday_p': 210, 'wkend_p': 260},
+             {'name': 'fishing-boat',     'capacity': 3, 'wkday_p': 215, 'wkend_p': 250},
+             {'name': 'swan-paddle-boat', 'capacity': 3, 'wkday_p': 25,  'wkend_p': 40}]
     
     # initial empty arrays for, to be added to db
     boats_db = []
@@ -28,15 +32,16 @@ with app.app_context():
     for b in boats:
         boat  = Boat(name = b['name'], capacity = b['capacity'])
         boats_db.append(boat)
-        boat2 = Boat(name = b['name'], capacity = b['capacity'])
-        boats_db.append(boat2)
         
     for x in range(len(boats_db)):
         time = Time(hour = random.choice(hours), day = random.choice(days))
         times_db.append(time)
 
-    for x in range(len(boats_db)):
-        bt = BoatTime(boat_id = (boats_db.index(random.choice(boats_db)) + 1), time_id = times_db.index(random.choice(times_db)) + 1)
+    for b in boats:
+        bt = BoatTime(boat_id = boats_db.index(random.choice(boats_db)) + 1,
+                       time_id = times_db.index(random.choice(times_db)) + 1,
+                       weekday_price = b['wkday_p'],
+                       weekend_price = b['wkend_p'])
         bts_db.append(bt)
     
     #  adds all instances to the db and commits it
