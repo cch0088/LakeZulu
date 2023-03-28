97f08df203c8ee8c97d278563772da0a9a53ae6d
Constantine Cherkashyn <cons1@protonmail.ch>
2023-03-28 16:09:49 -0400

    Table working

--- client/src/components/Reserve.js
+++ client/src/components/Reserve.js
@@ -2,15 +2,24 @@ import React from "react";
 
 function Reserve({schedule}) {
   let days = [];
+  let times = [];
+  let boats = [];
 
   for (let record of schedule) {
     days.push(record['day']);
+    times.push(record['hour']);
+    boats.push(record['boats']);
   }
 
   return (
     <div className="content">
       The following days are available at this time:
-      {days.map((day, index) => {return <p key={index}>{day}</p>})}
+      <div className="div-table">
+        <div className="div-table-item">Day</div><div className="div-table-item">Time</div><div className="div-table-item">Available Units</div>
+        {boats.map((boat, index) => {
+          return <div className="div-table-item" key={index}>{days[index]}<div>{times[index]}</div><div>{boat.length} boats</div></div>
+        })}
+      </div>
     </div>
   )
 }
