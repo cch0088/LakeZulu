9cda3bd991795a11a77cf7e12cdbdf7432361422
Constantine Cherkashyn <cons1@protonmail.ch>
2023-03-28 07:42:56 -0400

    Update

--- client/src/components/Reserve.js
+++ client/src/components/Reserve.js
@@ -4,13 +4,24 @@ import Login from "./Login"
 function Reserve({check_user}) {
   if (check_user)
   {
-      const username = Object.values(check_user)[1]
+    const username = Object.values(check_user)[1]
+    
+    const API = "/times";
 
-      return (
-          <div className="content">
-              Welcome back {username}! Please pick a day to get started.
-          </div>
-      )
+    fetch(API).then(resp => resp.json())
+    .then(data => {
+      for (let record of data) {
+        console.log(record['day']);
+        console.log(record['hour']);
+        console.log(record['boats']);
+      }
+    });
+    
+    return (
+      <div className="content">
+        Welcome back {username}! The following days are available at this time:
+      </div>
+    )
   }
   else
   {
