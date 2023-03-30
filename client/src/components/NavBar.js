7db15b959752ae27a3ca6c202ff2efe81b8651e2
Eli Moshe <moshebogo@gmail.com>
2023-03-30 14:22:34 -0400

    fixed dark button

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,22 +1,19 @@
 import { NavLink } from "react-router-dom";
 import React from "react";
 
-function NavBar({ setDarkMode }) {
+function NavBar() {
 
 return(
     <div className="navigation">
-        <NavLink to="/create_res">Make Reservations</NavLink>
-        <NavLink to="/Events">Upcoming Events</NavLink>
-        <NavLink to="/SpecialPkgs">Special Packages</NavLink>
-        <NavLink to="/BoatsList">Our Boat Inventory</NavLink>
-        <NavLink to="/ContactPage">Contact Information</NavLink>
+        <NavLink to="/create_res">Reservations</NavLink>
+        <NavLink to="/Events">Events</NavLink>
+        <NavLink to="/SpecialPkgs">Packages</NavLink>
+        <NavLink to="/BoatsList">Boats</NavLink>
+        <NavLink to="/ContactPage">Contact Us</NavLink>
         <button  id="darkMode"
-                 onClick={ (e) => {
-            // sets a class on the whole body, so the *entire* page has dark mode
-            document.querySelector('body').classList.toggle('darkMode')
-            (setDarkMode(prev => !prev))
-        }}     
-             >Dark Mode</button>
+                 onClick={ (e) => { // sets a class on the whole body, so the *entire* page has dark mode
+                                    document.querySelector('body').classList.toggle('darkMode')}}     
+                                    >Dark Mode</button>
      </div>
     )
 }
