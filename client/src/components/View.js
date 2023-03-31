d1e34ecc524dfae441b2c3a152ea86b77cd6dac7
Constantine Cherkashyn <cons1@protonmail.ch>
2023-03-30 21:06:15 -0400

    Reservation system working

--- client/src/components/View.js
+++ client/src/components/View.js
@@ -1,24 +1,77 @@
 import React from "react";
 
-function View({schedule}) {
+function View({schedule, pricing, username}) {
 
-  
+  // de-construct the schedule object into individual indexes for populating table
+  let days = [];
+  let times = [];
+  let boats = [];
+  let time_id = []; // used for price lookup
+  let price_matrix = [];
+
+  // de-construct the pricing object and reconstruct into price matrix array
+  for (let record of pricing) {
+    price_matrix.push([record['boat_id'] + ':' + record['time_id'], 
+                        record['id'] + ':' + record['price'] + ':' + record['reserved']]);
+  }
+
+  for (let record of schedule) {
+    days.push(record['day']);
+    times.push(record['hour']);
+    boats.push(record['boats']);
+    time_id.push(record['id']);
+  }
+
+  function queryBoatTimes(boat_id, time_id, get_id) {
+    const list_price = price_matrix.filter(([key, value]) => key === `${boat_id}:${time_id}`);
+    if (get_id === 1)
+    {
+      return list_price[0][1].split(':')[0]; // output the ID field in boat_times table
+    }
+    else if (get_id === 2)
+    {
+      return list_price[0][1].split(':')[2]; // output the reserve field in boat_times table
+    }
+    else
+    {
+      return list_price[0][1].split(':')[1]; // output the price field
+    }
+  }
 
   return (
     <div className="content">
+      <div>Viewing all reservations for {Object.values(username)[1]}</div>
       <div className="div-table">
         <div className="div-table-heading-title">
           <div className="div-table-cell">Day</div>
           <div className="div-table-cell">Time</div>
           <div className="div-table-cell">Boat</div>
+          <div className="div-table-cell">Capacity</div>
+          <div className="div-table-cell">Price</div>
         </div>
-          {/* {boats.map((boat, index) => {
-          return (
-            <div className="div-table-heading" key={index} id={index} onClick={(e) => handleTimeSelection(e)}>
-              {(days[index] === filterDay) ? (<div id={index} className="div-table-cell">{days[index]}</div>) : null}
-              {(days[index] === filterDay) ? (<div id={index} className="div-table-cell">{times[index]}</div>) : null}
-              {(days[index] === filterDay) ? (<div id={index} className="div-table-cell">{boat.length} boats</div>) : null}
-            </div>)})} */}
+        {boats.map((boat, index) => {
+          const arr = boat.map((b) => {
+            if (queryBoatTimes(b['id'], time_id[index], 2) === Object.values(username)[1]) {
+              const price = (queryBoatTimes(b['id'], time_id[index], 0))
+              return [
+                b['name'], 
+                b['capacity'], 
+                times[index], 
+                days[index],
+                price];
+            }
+            else {
+              return null;
+            }})
+          return ((arr[1] === null) ? null : (
+          <div className="div-table-heading" key={index} id={index}>
+            <div id={index} className="div-table-cell">{arr[1][3]}</div>
+            <div id={index} className="div-table-cell">{arr[1][2]}</div>
+            <div id={index} className="div-table-cell">{arr[1][0]}</div>
+            <div id={index} className="div-table-cell">{arr[1][1]}</div>
+            <div id={index} className="div-table-cell">${arr[1][4]}</div>
+          </div>));
+        })}
       </div>
     </div>);
 }
